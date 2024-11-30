074aadf4a07cf8ee9e443377c506bc81edb17a23
Olly Attwood <oliverattwood@aol.com>
2024-11-30 22:31:37 +0000

    Implemented custom table widget

---
+++ UI/custom_table.py
@@ -0,0 +1,81 @@
+import customtkinter
+from tkinter import StringVar
+
+# class to create a table widget
+class CustomTable(customtkinter.CTkFrame):
+    def __init__(self, master, data, columns, font_size=15, header_colour="black",
+                 data_colour="grey", cell_hover_colour="gray60",
+                 selected_row_colour = "SteelBlue1"):
+        super().__init__(master)
+
+        self.data = data
+        self.columns = columns
+        self.font = (None, font_size)
+        self.cell_hover_colour = cell_hover_colour
+        self.data_colour = data_colour
+        self.selected_row_colour = selected_row_colour
+        self.selected_row = None
+
+        # verify data is correct
+        self.check_is_data_correct_shape()
+
+        # display table
+        combined_data = [columns] + data
+
+        for row_num, row in enumerate(combined_data):
+            for col_num, col in enumerate(row):
+                cell = customtkinter.CTkEntry(self, textvariable=StringVar(self, col),
+                                              font=self.font, state="disabled", fg_color=data_colour)
+                cell.grid(row=row_num, column=col_num)
+
+                # header row
+                if row_num == 0:
+                    cell.configure(fg_color=header_colour)
+                # data rows
+                else:
+                    # hover bindings
+                    cell.bind("<Enter>", lambda event: self.mouse_hover_data_cell(event, cell_hover_colour))
+                    cell.bind("<Leave>", lambda event: self.mouse_hover_data_cell(event, data_colour))
+                    #click binding
+                    cell.bind("<Button-1>", self.click_row)
+
+    def check_is_data_correct_shape(self):
+        if len(self.data) == 0:
+            return
+
+        num_cols_in_data = len(self.data[0])
+
+        for row in self.data:
+            if len(row) != num_cols_in_data:
+                raise Exception("Rows in data are different lengths")
+
+        if len(self.columns) != num_cols_in_data:
+            raise Exception("Number of column names is different to data")
+
+    def change_row_colour(self, row_num, grid_info, colour):
+        cells_in_row = grid_info["in"].grid_slaves(row=row_num)
+
+        for cell in cells_in_row:
+            cell.configure(fg_color=colour)
+
+    def click_row(self, event):
+        # must use event.widget.master below instead of just event.widget
+        # see https://stackoverflow.com/questions/75361805/customtkinter-why-does-this-event-widget-lose-the-proper-grid-information
+        grid_info = event.widget.master.grid_info()
+        row_num = grid_info["row"]
+
+        if self.selected_row:
+            self.change_row_colour(self.selected_row, grid_info, self.data_colour)
+
+        self.selected_row = row_num
+        self.change_row_colour(row_num, grid_info, self.selected_row_colour)
+        print(row_num)
+
+    def mouse_hover_data_cell(self, event, colour):
+        # must use event.widget.master below instead of just event.widget
+        # see https://stackoverflow.com/questions/75361805/customtkinter-why-does-this-event-widget-lose-the-proper-grid-information
+        grid_info = event.widget.master.grid_info()
+        row_num = grid_info["row"]
+
+        if row_num != self.selected_row:
+            self.change_row_colour(row_num, grid_info, colour)
--- UI/view_items_frame.py
+++ UI/view_items_frame.py
@@ -1,8 +1,14 @@
 import customtkinter
+from tkinter import StringVar
+from custom_table import CustomTable
 
 class ViewItemsFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        lbl = customtkinter.CTkLabel(self, text="View Items!", font=(None, 100))
-        lbl.grid(row=0, column=0)
+        test_data = [["necklace", 1, 3],
+                     ["earrings", 2, 1],
+                     ["bauble", 3, 2]]
+        test_cols = ["Item", "ID", "Stock"]
+        self.table = CustomTable(self, test_data, test_cols)
+        self.table.grid(row=0, column=0)
