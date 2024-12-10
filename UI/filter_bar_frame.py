1ffa40e4055439cc4a9bba7688f1c9174cbd2913
Olly Attwood <oliverattwood@aol.com>
2024-12-10 16:21:48 +0000

    Spaces added to comments

--- UI/filter_bar_frame.py
+++ UI/filter_bar_frame.py
@@ -5,14 +5,14 @@ class FilterBarFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        #padding vars
+        # padding vars
         self.lbl_padx = 10
         self.widget_padx= (0,10)
         self.pady = 10 # pady for both labels and other widgets
-        #widget position counter
+        # widget position counter
         self.current_widget_num = 0
 
-        #item type
+        # item type
         product_component_lbl = customtkinter.CTkLabel(self, text="Item Type:")
         self.add_label(product_component_lbl)
         product_component_vals = ["Product", "Component"]
@@ -20,37 +20,37 @@ class FilterBarFrame(customtkinter.CTkFrame):
         product_component_switch.set(product_component_vals[0])
         self.add_input_widget(product_component_switch)
 
-        #name
+        # name
         name_lbl = customtkinter.CTkLabel(self, text="Name:")
         self.add_label(name_lbl)
         name_entry = customtkinter.CTkEntry(self)
         self.add_input_widget(name_entry)
 
-        #design
+        # design
         design_lbl = customtkinter.CTkLabel(self, text="Design:")
         self.add_label(design_lbl)
         design_dropdown = customtkinter.CTkOptionMenu(self, values = ["", "Design1", "Design2", "Design3"])
         self.add_input_widget(design_dropdown)
 
-        #design theme
+        # design theme
         design_theme_lbl = customtkinter.CTkLabel(self, text="Theme:")
         self.add_label(design_theme_lbl)
         design_theme_dropdown = customtkinter.CTkOptionMenu(self, values = ["", "Theme1", "Theme2", "Theme3"])
         self.add_input_widget(design_theme_dropdown)
 
-        #type
+        # type
         type_lbl = customtkinter.CTkLabel(self, text="Type:")
         self.add_label(type_lbl)
         type_dropdown = customtkinter.CTkOptionMenu(self, values = ["", "Type1", "Type2", "Type3"])
         self.add_input_widget(type_dropdown)
 
-        #sub-type
+        # sub-type
         sub_type_lbl = customtkinter.CTkLabel(self, text="Sub-type:")
         self.add_label(sub_type_lbl)
         sub_type_dropdown = customtkinter.CTkOptionMenu(self, values = ["", "Sub-type1", "Sub-type2", "Sub-type3"])
         self.add_input_widget(sub_type_dropdown)
 
-        #colour
+        # colour
         colour_lbl = customtkinter.CTkLabel(self, text="Colour:")
         self.add_label(colour_lbl)
         colour_dropdown = customtkinter.CTkOptionMenu(self, values = ["", "Colour1", "Colour2", "Colour3"])
