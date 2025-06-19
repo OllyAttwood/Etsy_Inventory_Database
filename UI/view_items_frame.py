c5b6853641085aa5c7b011c2f51e403fe1cc0d9e
Olly Attwood <oliverattwood@aol.com>
2025-06-19 15:00:56 +0100

    ViewItemsFrame now loads all product data initially from the DB

--- UI/view_items_frame.py
+++ UI/view_items_frame.py
@@ -12,11 +12,9 @@ class ViewItemsFrame(customtkinter.CTkFrame):
         filter_bar = FilterBarFrame(self)
         filter_bar.grid(row=0, column=0, pady=10)
 
-        test_data = [["necklace", 1, 3],
-                     ["earrings", 2, 1],
-                     ["bauble", 3, 2]]
-        test_cols = ["Item", "ID", "Stock"]
-        self.table = CustomTable(self, test_data, test_cols)
+        full_data = self.presenter.get_filtered_products()
+        #TODO convert column names into more user-friendly names (capitalise, remove underscores etc)
+        self.table = CustomTable(self, full_data["data"], full_data["column_names"])
         self.table.grid(row=1, column=0)
 
         self.grid_columnconfigure(0, weight=1)
--- presenter.py
+++ presenter.py
@@ -3,3 +3,6 @@ from database_manager import DatabaseManager
 class Presenter:
     def __init__(self):
         self.db_manager = DatabaseManager()
+
+    def get_filtered_products(self):
+        return self.db_manager.view_filtered_products()
