15e7c89e32691e942cad45096976007afb574865
Olly Attwood <oliverattwood@aol.com>
2025-07-08 17:21:34 +0100

    Added docstrings to the functions in db_tab_view.py

--- UI/db_tab_view.py
+++ UI/db_tab_view.py
@@ -4,6 +4,7 @@ from UI.add_new_item_frame import AddNewItemFrame
 from UI.low_stock_frame import LowStockFrame
 
 class DBTabView(customtkinter.CTkTabview):
+    """The main view which controls which of the main frames is being displayed"""
     VIEW_ITEMS_FRAME_INDEX = 0
     ADD_NEW_ITEM_FRAME_INDEX = 1
     LOW_STOCK_FRAME_INDEX = 2
@@ -39,14 +40,18 @@ class DBTabView(customtkinter.CTkTabview):
         return LowStockFrame(master=self.tab(self.TAB_NAMES[self.LOW_STOCK_FRAME_INDEX]), presenter=self.presenter)
 
     def display_frame_in_grid(self, frame):
+        """Displays the given frame"""
         frame.grid(row=0, column=0, sticky="nesw")
 
     def _reload_tab_contents(self, tab_num, replacement_frame):
+        """Replaces the specified frame in this tab_view with a new one"""
         frame = self.tab_frames[tab_num]
         frame.destroy()
         self.display_frame_in_grid(replacement_frame)
 
     def _reload_view_items_frame(self, display_components):
+        """Refreshes the ViewItemsFrame - display_components controls whether the
+        components are initially displayed rather than the default products"""
         replacement_frame = self.create_view_items_frame()
         self._reload_tab_contents(self.VIEW_ITEMS_FRAME_INDEX, replacement_frame)
 
@@ -54,10 +59,12 @@ class DBTabView(customtkinter.CTkTabview):
             replacement_frame.filter_bar.display_components()
 
     def _reload_add_new_item_frame(self):
+        """Refreshes the AddNewItemFrame"""
         replacement_frame = self.create_add_new_item_frame()
         self._reload_tab_contents(self.ADD_NEW_ITEM_FRAME_INDEX, replacement_frame)
 
     def _reload_low_stock_frame(self):
+        """Refreshes the LowStockFrame"""
         replacement_frame = self.create_low_stock_frame()
         self._reload_tab_contents(self.LOW_STOCK_FRAME_INDEX, replacement_frame)
 
