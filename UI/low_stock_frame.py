43375240f9b1fcae5a060a8914e458e70fa1da0a
Olly Attwood <oliverattwood@aol.com>
2025-08-03 15:19:20 +0100

    Reduced repetitive code in LowStockFrame by introducing a helper function

--- UI/low_stock_frame.py
+++ UI/low_stock_frame.py
@@ -4,28 +4,28 @@ from UI import config
 
 class LowStockFrame(customtkinter.CTkFrame):
     """A frame which displays the items which have a stock level less than or equal to their stock warning value"""
-    
+
     def __init__(self, master, presenter):
         super().__init__(master)
         self.presenter = presenter
-        space_between_tables = 50
 
         # product table
-        products_label = self.create_label("Low Stock Products")
-        products_label.grid(row=0, column=0, pady=config.WIDGET_Y_PADDING)
         low_stock_product_data = self.presenter.get_low_stock_items(products=True, components=False)
-        product_table = CustomTable(self, low_stock_product_data["data"], low_stock_product_data["column_names"])
-        product_table.grid(row=1, column=0, sticky="nsew", pady=(0, space_between_tables)) #the pady is to create a gap between the products and components
+        self.add_table("Low Stock Products", 0, low_stock_product_data)
 
         #component table
-        components_label = self.create_label("Low Stock Components")
-        components_label.grid(row=2, column=0, pady=config.WIDGET_Y_PADDING)
         low_stock_component_data = self.presenter.get_low_stock_items(products=False, components=True)
-        component_table = CustomTable(self, low_stock_component_data["data"], low_stock_component_data["column_names"])
-        component_table.grid(row=3, column=0, sticky="nsew",)
+        self.add_table("Low Stock Components", 2, low_stock_component_data)
 
         self.grid_columnconfigure(0, weight=1) #puts everything in the middle
 
     def create_label(self, text, font_size=25):
         """Creates labels with the same appearance"""
         return customtkinter.CTkLabel(self, text=text, font=(None, font_size))
+
+    def add_table(self, title, starting_row_index, item_data, space_between_tables=50):
+        """Adds a table to the frame at the given starting row"""
+        title_label = self.create_label(title)
+        title_label.grid(row=starting_row_index, column=0, pady=config.WIDGET_Y_PADDING)
+        table = CustomTable(self, item_data["data"], item_data["column_names"])
+        table.grid(row=starting_row_index+1, column=0, sticky="nsew", pady=(0, space_between_tables)) #the pady is to create a gap between the different tables
