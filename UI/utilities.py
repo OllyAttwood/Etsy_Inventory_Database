b1e6d6b565bb68899fabbca9715a66f6542cf2bb
Olly Attwood <oliverattwood@aol.com>
2025-08-03 15:46:43 +0100

    Renamed add_empty_string_option_and_alphabetise() function

--- UI/utilities.py
+++ UI/utilities.py
@@ -1,9 +1,9 @@
 """Functions that are used in more than one class"""
 
-def add_empty_string_option_and_alphabetise(list_of_options_lists):
+def prepare_dropdown_options_lists(list_of_options_lists):
     """
     Inserts an empty string at the beginning of each of the lists, to use as the default (unfiltered) value for a dropdown menu.
-    Also orders the list alphabetically.
+    Also orders the list alphabetically. Does these actions in-place (i.e. modifies existing lists)
     """
     for options in list_of_options_lists:
         options.sort(key=str.lower) # alphabetise list
